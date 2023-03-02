e8ded05b9f6085f64380aaf0252708fd78fff732
tptin76 <tptin.76@gmail.com>
2023-03-02 23:47:56 +0700

    ECS handle system manager

--- src/ECS/Systems/RenderSystem/RenderSystem.cpp
+++ src/ECS/Systems/RenderSystem/RenderSystem.cpp
@@ -1,4 +1,7 @@
 #include "RenderSystem.h"
+#include "../../Entity/EntityManager.h"
+#include "../../Components/Components.h"
+#include "../../Components/ComponentManager.h"
 
 namespace ngin2D {
 RenderSystem::RenderSystem()
@@ -6,7 +9,10 @@ RenderSystem::RenderSystem()
 
 void RenderSystem::update(float dt)
 {
-
+    for(Entity entity: EntityManager::instance()->getEntities())
+    {
+       std::cout << entity.id <<  " " <<  ComponentManager::instance()->hasComponentType<SpriteComponent>(entity.components) << std::endl;
+    }
 }
 
 void RenderSystem::render()
