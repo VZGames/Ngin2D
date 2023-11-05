e67054727b4050872d0cf1a73b8e18479a77354a
Tran Phuoc Tin <tptin.76@gmail.com>
2023-11-05 13:24:24 +0700

    update

--- src/engine/ECS/Systems/MovementSystem/CMovementSystem.cpp
+++ src/engine/ECS/Systems/MovementSystem/CMovementSystem.cpp
@@ -24,7 +24,19 @@ void CMovementSystem::update(std::vector<CEntity *> &entities, float dt)
     // do update for each entity
     for(CEntity *entity: *m_entities)
     {
+        bool hasPosition = entity->hasComponent<SPositionComponent>();
+        bool hasMotion   = entity->hasComponent<SMotionComponent>();
+        bool hasCamera   = entity->hasComponent<SCameraComponent>();
+//        UNUSED(hasCamera)
+//        if(!(hasPosition && hasMotion)) return;
+//        auto position = entity->getComponent<SPositionComponent>();
+//        Offset offset = Ngin::CCamera::instance()->offset();
+//        position->x -= offset.getX();
+//        position->y -= offset.getY();
+//        MORGAN_DEBUG("%f %f ", position->x, position->y)
+
         entity->motionUpdate(dt);
+
     }
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,6 @@ int main(int argc, char *argv[])
         {
             &player,
             &enemy,
-            &enemy2
         };
 
     std::vector<AScene*> scenes
