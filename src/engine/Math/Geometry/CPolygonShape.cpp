be3eb7d415d5712281562830b851b58ea3881dc7
Tran Phuoc Tin <tptin.76@gmail.com>
2023-11-25 16:48:20 +0700

    Doing collision

--- src/engine/Math/Geometry/CPolygonShape.cpp
+++ src/engine/Math/Geometry/CPolygonShape.cpp
@@ -36,7 +36,7 @@ std::pair<float, float> CPolygonShape::projection(Vector2DF axis)
     float min = std::numeric_limits<float>::infinity();
     float max = -std::numeric_limits<float>::infinity();
 
-    for (Vector2DF &vertex: m_vertices)
+    for (const Vector2DF &vertex: m_vertices)
     {
         float dotProduct = axis.dotProduct(Vector2D<float>(m_x, m_y) + vertex);
         min = std::min(min, dotProduct);
