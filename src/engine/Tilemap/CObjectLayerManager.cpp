acceb55ba26a845fba1346dcafb9092731e75ce0
Tran Phuoc Tin <tptin.76@gmail.com>
2023-12-31 00:36:06 +0700

    Add object layer manager

--- src/engine/Tilemap/CObjectLayerManager.cpp
+++ src/engine/Tilemap/CObjectLayerManager.cpp
@@ -8,13 +8,24 @@ CObjectLayerManager::CObjectLayerManager()
 
 void CObjectLayerManager::push(TmxObject &&object)
 {
+    CPolygonShape shape;
+    shape.pushVertex(object.x, object.y);
+    shape.pushVertex(object.x + object.width, object.y + object.y);
+
+    m_box.push_back(std::move(shape));
     m_objects.push_back(object);
 }
 
-std::vector<TmxObject> CObjectLayerManager::objects() const
+const std::vector<TmxObject> *CObjectLayerManager::objects() const
 {
-    return m_objects;
+    return &m_objects;
+}
+
+const std::vector<CPolygonShape> *CObjectLayerManager::box() const
+{
+    return &m_box;
 }
 END_NAMESPACE
+
 
 
--- src/engine/Tilemap/CObjectLayerManager.h
+++ src/engine/Tilemap/CObjectLayerManager.h
@@ -3,17 +3,20 @@
 
 #include "CommonDefine.h"
 #include "TilemapParser/TmxFormat.h"
+#include "Geometry/CPolygonShape.h"
 
 BEGIN_NAMESPACE(engine)
 class CObjectLayerManager
 {
 private:
     std::vector<TmxObject> m_objects;
+    std::vector<CPolygonShape> m_box;
 public:
     CObjectLayerManager();
 
     void push(TmxObject&&);
-    std::vector<TmxObject> objects() const;
+    const std::vector<TmxObject> *objects() const;
+    const std::vector<CPolygonShape> *box() const;
 };
 END_NAMESPACE
 
